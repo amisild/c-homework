3b37ab15a4bb09cb652e5afdc62ad140eccd3d90
amisild <56818531+amisild@users.noreply.github.com>
2020-09-17 10:08:12 +0300

    Add files via upload

---
+++ homework1.2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double palgad [] = {923.3, 1033, 3928.5, 509, 999.9, 1000, 1709};
+
+    int length = sizeof(palgad)/sizeof(double);
+
+    for (int i = 0; i < length; i++) {
+        if (palgad[i] < 1000) {
+                cout << "Pärast palgatõusu on su palk " << palgad[i] * 1.05 << " eurot." << endl;
+        }
+    }
+
+    //palgatõusu arvutamine user inputi puhul
+    cout << "Mis on su palk? " << endl;
+    double palk;   
+    cin >> palk;
+
+    if (palk < 1000) {
+        double raise = palk * 1.05; //palgatõus
+        cout << "Sinu uueks palgaks on " << raise << " eurot." << endl;
+    } else {
+        cout << "Selle palgaga palgatõusu ei saa." << endl;
+    }
+}
