7e4ea19f78239282da281e026ea0252c6a565686
amisild <56818531+amisild@users.noreply.github.com>
2020-09-17 10:46:30 +0300

    Update homework1.1.cpp

--- homework1.1.cpp
+++ homework1.1.cpp
@@ -4,28 +4,23 @@
 
 using namespace std;
 
+int r; //r = ringi raadius
+int a = 2 * r; //a on ruudu külg
+int ruudu_pindala = a * a;
+int ruudu_umbermoot = 2 * a;
+double ringi_pindala = M_PI * r * r;
+double ringi_umbermoot = 2 * M_PI * r;
+
 int main()
 {
-    int r; //r = ringi raadius
-
     cout << "Sisesta ringi raadius: " << endl;
     cin >> r;
-
-    int a = 2 * r; //a on ruudu külg
-
+    
     cout << "Sinu valitud raadiuse puhul on ruudu külg " << a << " ühiku pikkusega." << endl;
-
-    int ruudu_pindala = a * a;
-    int ruudu_umbermoot = 2 * a;
-    double ringi_pindala = M_PI * r * r;
-    double ringi_umbermoot = 2 * M_PI * r;
 
     cout << "Sa valisid raadiuseks " << r << "." << endl;
     cout << "Sellise raadiuse puhul on ruudu pindala: " << ruudu_pindala << endl;
     cout << "Ruudu ümbermõõt: " << ruudu_umbermoot << endl;
     cout << "Ringi pindala: " << ringi_pindala << endl;
     cout << "Ringi ümbermõõt: " << ringi_umbermoot << endl;
-
-
-
 }
