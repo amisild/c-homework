4fd1b733d430575fcaa6309c46c9f006428324bc
amisild <56818531+amisild@users.noreply.github.com>
2020-09-17 11:30:40 +0300

    Update homework1.2.cpp

--- homework1.2.cpp
+++ homework1.2.cpp
@@ -2,28 +2,36 @@
 
 using namespace std;
 
-double palgad [] = {923.3, 1033, 3928.5, 509, 999.9, 1000, 1709};
-int length = sizeof(palgad)/sizeof(double);
+double salaries [] = {923.3, 1033, 3928.5, 509, 999.9, 1000, 1709};
+int length = sizeof(salaries)/sizeof(double);
 
-//palgatõusu arvutamine user inputi puhul
-double palk;
-double raise = palk * 1.05; //palgatõus
-
-int main()
-{
+double palkade_massiiv() {
     for (int i = 0; i < length; i++) {
-        if (palgad[i] < 1000) {
-                cout << "Pärast palgatõusu on su palk " << palgad[i] * 1.05 << " eurot." << endl;
+        if (salaries[i] < 1000) {
+                cout << "Pärast palgatõusu on su palk " << salaries[i] * 1.05 << " eurot." << endl;
         }
     }
+}
 
-    //palgatõus user inputiga
-    cout << "Mis on su palk? " << endl;
-    cin >> palk;
-    
-    if (palk < 1000) {
+//palgatõusu arvutamine user inputi puhul
+double salary;
+double raise = salary * 1.05; //palgatõus
+
+double raise_user_input() {
+    if (salary < 1000) {
         cout << "Sinu uueks palgaks on " << raise << " eurot." << endl;
     } else {
         cout << "Selle palgaga palgatõusu ei saa." << endl;
     }
+}
+
+int main()
+{
+    palkade_massiiv();
+
+    //palgatõus user inputiga
+    cout << "Mis on su palk? " << endl;  
+    cin >> salary;
+    raise_user_input();
+    
 }
