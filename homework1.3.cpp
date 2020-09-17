029ac3e02a1dabb7ba5b7e9d2f079e170d205a43
amisild <56818531+amisild@users.noreply.github.com>
2020-09-17 10:21:22 +0300

    Update homework1.3.cpp

--- homework1.3.cpp
+++ homework1.3.cpp
@@ -1,9 +1,3 @@
-/*
-Kirjutada programm, mis võtab temperatuuri täisarvuna ja kuvab selle kohta tekstilise hinnangu järgmise
-reegli alusel: kui temperatuur on suurem kui 25, siis on väljund ”hot!”, juhul kui temperatuur on väiksem
-kui 10, siis on väljund ”cold!”, muul juhul on väljund ”warm!”.
-*/
-
 #include <iostream>
 
 using namespace std;
