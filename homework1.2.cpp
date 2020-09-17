5805582daccffa66ad0c94ae5c354200fd3476c8
amisild <56818531+amisild@users.noreply.github.com>
2020-09-17 10:51:26 +0300

    Update homework1.2.cpp

--- homework1.2.cpp
+++ homework1.2.cpp
@@ -2,24 +2,25 @@
 
 using namespace std;
 
+double palgad [] = {923.3, 1033, 3928.5, 509, 999.9, 1000, 1709};
+int length = sizeof(palgad)/sizeof(double);
+
+//palgatõusu arvutamine user inputi puhul
+double palk;
+double raise = palk * 1.05; //palgatõus
+
 int main()
 {
-    double palgad [] = {923.3, 1033, 3928.5, 509, 999.9, 1000, 1709};
-
-    int length = sizeof(palgad)/sizeof(double);
-
     for (int i = 0; i < length; i++) {
         if (palgad[i] < 1000) {
                 cout << "Pärast palgatõusu on su palk " << palgad[i] * 1.05 << " eurot." << endl;
         }
     }
 
-    //palgatõusu arvutamine user inputi puhul
+    //palgatõus user inputiga
     cout << "Mis on su palk? " << endl;
-    double palk;   
     cin >> palk;
-    double raise = palk * 1.05; //palgatõus
-
+    
     if (palk < 1000) {
         cout << "Sinu uueks palgaks on " << raise << " eurot." << endl;
     } else {
